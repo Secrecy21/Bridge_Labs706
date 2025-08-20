9d977864fd6e2cb7c5d9412193bc2107a08a4619
satbi <satbir888rajat@gmail.com>
2025-08-20 14:58:23 +0530

    Submission  of Programming elements assessment

---
+++ Core_Programming/Submission_of_programing_elements/Find_Harry_Age.java
@@ -0,0 +1,12 @@
+package Core_Programming.Submission_of_programing_elements;
+
+
+public class Find_Harry_Age {
+    public static void main(String[] args) {
+        int birthYear = 2000;
+        int currentYear = 2024;
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
